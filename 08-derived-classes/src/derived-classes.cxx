0f0bc8b0dcb0baf3767be85c3f4f3a2ad3991d0f
Matthias Moeller <m.moller@tudelft.nl>
2015-09-22 14:42:03 +0200

    Added example 08-derived-class for session 3.

---
+++ 08-derived-classes/src/derived-classes.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Base {
+private:
+  int my_a;
+
+public:
+  // Constructor of Base class
+  Base(int a){ my_a=a; }
+};
+
+class Super: public Base {
+private:
+  int my_b;
+  
+public:
+  // Constructor of Super class delegating part of the work to the
+  // constructor of Base class
+  Super(int a, int b): Base(a) { my_b=b; }
+};
+
+// Main program
+int main() {
+  Base  b(1);
+  Super s(2,3);
+}
