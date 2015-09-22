3c794c428b3c71dc132e6d391a9ba0de9b191f40
Joost van Zwieten <joostvanzwieten@gmail.com>
2015-09-22 13:28:03 +0200

    add solution homework2

---
+++ H02-fraction-solved/src/fraction.cxx
@@ -0,0 +1,89 @@
+#include <iostream>
+
+int gcd(int a, int b)
+{
+    while (b)
+    {
+        int c = a%b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+class Fraction
+{
+public:
+
+    Fraction(int _n, int _d=1): n(_n), d(_d) {}
+    Fraction(int _n, int _d, bool _normalize) : Fraction(_n, _d)
+    {
+        if (_normalize)
+            normalize();
+    }
+
+    void normalize()
+    {
+        int f = gcd(n, d);
+        n /= f;
+        d /= f;
+        if (d < 0)
+        {
+            n *= -1;
+            d *= -1;
+        }
+    }
+
+    Fraction normalized() const
+    {
+        Fraction f(*this);
+        f.normalize();
+        return f;
+    }
+
+    Fraction operator-() const { return Fraction(-n, d); }
+
+    explicit operator double() const { return double(n)/double(d); }
+
+    int n, d;
+};
+
+Fraction operator+(const Fraction &l, const Fraction &r)
+{
+    return Fraction(l.n*r.d+r.n*l.d, l.d*r.d);
+}
+
+Fraction operator-(const Fraction &l, const Fraction &r)
+{
+    return Fraction(l.n*r.d-r.n*l.d, l.d*r.d);
+}
+
+Fraction operator*(const Fraction &l, const Fraction &r)
+{
+    return Fraction(l.n*r.n, l.d*r.d);
+}
+
+Fraction operator/(const Fraction &l, const Fraction &r)
+{
+    return Fraction(l.n*r.d, l.d*r.n);
+}
+
+std::ostream &operator<<(std::ostream &os, const Fraction &f)
+{
+    return os << f.n << "/" << f.d;
+}
+
+int main()
+{
+    using namespace std;
+    Fraction a(2, -3);
+    Fraction b(1, 5);
+
+    cout << "a = " << a << endl;
+    cout << "b = " << a << endl;
+
+    cout << "3*a+b = " << 3*a+b << " = " << (3*a+b).normalized() << endl;
+    cout << "a-b = " << a-b << " = " << (a-b).normalized() << endl;
+    cout << "5*a*b = " << 5*a*b << " = " << (5*a*b).normalized() << endl;
+    cout << "3*a/b = " << 3*a/b << " = " << (3*a/b).normalized() << endl;
+}
